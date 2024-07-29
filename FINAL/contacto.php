bd91ef98866102af5f7dba21a29a84b9b0ec34cb
Nachociro <ifigueroaferrari@gmail.com>
2024-07-28 22:53:02 -0300

    Update contacto.php

--- FINAL/contacto.php
+++ FINAL/contacto.php
@@ -19,21 +19,21 @@
                                 <div class="container">
                                     <div class="row">
                                         <div class="col">
-                                            <label for="nombre">Nombre:</label> <br>
-                                            <input type="text" name="nombre" id="nombre" placeholder="Ingrese su nombre" title="Ingrese su nombre aquí" maxlength="60" required>
+                                            <label class="form-label" for="nombre">Nombre:</label> <br>
+                                            <input class="form-input" type="text" name="nombre" id="nombre" placeholder="Ingrese su nombre" title="Ingrese su nombre aquí" maxlength="60" required>
                                         </div>
                                         <div class="col">
-                                            <label for="apellido">Apellido:</label> <br>
-                                            <input type="text" name="apellido" id="apellido" placeholder="Ingrese su apellido" title="Ingrese su apellido aquí" maxlength="30" required>
+                                            <label class="form-label "for="apellido">Apellido:</label> <br>
+                                            <input class="form-input" type="text" name="apellido" id="apellido" placeholder="Ingrese su apellido" title="Ingrese su apellido aquí" maxlength="30" required>
                                         </div>
                                     </div>
                                     <div class="row">
                                         <div class="col">
-                                            <label for="mail">Mail:</label> <br>
-                                            <input type="email" name="mail" id="mail" placeholder="Ingrese su mail" title="Ingrese su mail aquí" maxlength="60" required>
+                                            <label class="form-label" for="mail">Mail:</label> <br>
+                                            <input class="form-input" type="email" name="mail" id="mail" placeholder="Ingrese su mail" title="Ingrese su mail aquí" maxlength="60" required>
                                         </div>
                                         <div class="col">
-                                            <label for="problema">Motivo de contacto:</label> <br>
+                                            <label  class="form-label" for="problema">Motivo de contacto:</label> <br>
                                                 <select id="problema" name="problema">
                                                     <option value="Problema técnico con mi iPhone">Problema técnico con mi iPhone</option>
                                                     <option value="Problema técnico con mi AppleWatch">Problema técnico con mi AppleWatch</option>
@@ -44,8 +44,8 @@
                                         </div>
                                     </div>
                                     <div class="row">
-                                        <label for="carga">Cargar archivo:</label>
-                                        <input id="carga" type="file" name="carga" required>
+                                        <label class="form-label" for="carga">Cargar archivo:</label>
+                                        <input class="form-input" id="carga" type="file" name="carga" required>
                                     </div>
                                     <div class="row">
                                         <div class="col centrado">
