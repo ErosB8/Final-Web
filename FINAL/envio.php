9f2f7700ba4fad97d16b9fa346e1485be48ecf58
Nachociro <ifigueroaferrari@gmail.com>
2024-07-28 23:05:36 -0300

    Update envio.php

--- FINAL/envio.php
+++ FINAL/envio.php
@@ -1,63 +1,73 @@
 <?php
     include_once("header.php");
+    include_once("conexion.php");
 ?>
-    <main class="mainc">
-        <div id="letrasenvio" class="container-fluid">
-            <div class="row">
-                <div class="col centrado mt25px">
-                    <h1>Muchas gracias!</h1>
-                </div>
+<main class="mainc">
+    <div id="letrasenvio" class="container-fluid">
+        <div class="row">
+            <div class="col centrado mt25px">
+                <h1>Muchas gracias!</h1>
             </div>
-            <div class="row">
-                <div class="col centrado mt25px">
-                    <p>Nuestro equipo se pondrá manos a la obra y en la brevedad se contactarán contigo</p>
-                </div>
+        </div>
+        <div class="row">
+            <div class="col centrado mt25px">
+                <p>Nuestro equipo se pondrá manos a la obra y en la brevedad se contactarán contigo</p>
             </div>
-            <div class="row">
-                <div class="col centrado">
-                    <h2>Hemos recibido:</h2>
-                </div>
+        </div>
+        <div class="row">
+            <div class="col centrado">
+                <h2>Hemos recibido:</h2>
             </div>
-            <div class="row">
-                <div class="col"></div>
-                <div class="col">
-                    <?php
-                        $nombre;
-                        $apellido;
-                        $mail;
-                        $problema;
-                        $carga;
-                        $temp;
-                        
+        </div>
+        <div class="row">
+            <div class="col"></div>
+            <div class="col">
+                <?php
+                
+                    $nombre = $apellido = $mail = $problema = $carga = $temp = "";
+
+                    if ($_SERVER["REQUEST_METHOD"] == "POST") {
                         if(isset($_POST['nombre'])){
-                            $nombre = $_POST['nombre'];
+                            $nombre = mysqli_real_escape_string($conexion, $_POST['nombre']);
                             print "<p><strong>Nombre</strong>: $nombre</p>";
                         }
                         if(isset($_POST['apellido'])){
-                            $apellido = $_POST['apellido'];
+                            $apellido = mysqli_real_escape_string($conexion, $_POST['apellido']);
                             print "<p><strong>Apellido</strong>: $apellido</p>";                        
                         }
                         if(isset($_POST['mail'])){
-                            $mail = $_POST['mail'];
+                            $mail = mysqli_real_escape_string($conexion, $_POST['mail']);
                             print "<p><strong>E-mail</strong>: $mail</p>";                       
                         }
                         if(isset($_POST['problema'])){
-                            $problema = $_POST['problema'];
+                            $problema = mysqli_real_escape_string($conexion, $_POST['problema']);
                             print "<p><strong>Motivo</strong>: $problema</p>";                        
                         }
+                        
                         
                         $carga = time().".jpg";
                         $temp = $_FILES['carga']['tmp_name'];
                         
-                        move_uploaded_file($temp,"recibido/$carga");
-                        
-                        print "<p><strong>Imagen</strong>:</p><img class='mb20px' src=recibido/$carga />";                       
-                    ?>
-                </div>
-                <div class="col"></div>
+                        if (move_uploaded_file($temp, "recibido/$carga")) {
+                            
+                            $sql = "INSERT INTO contactos (nombre, apellido, mail, problema, carga) VALUES ('$nombre', '$apellido', '$mail', '$problema', '$carga')";
+                            if (mysqli_query($conexion, $sql)) {
+                                echo "¡Datos enviados exitosamente!";
+                            } else {
+                                echo "Error al insertar datos: " . mysqli_error($conexion);
+                            }
+                        } else {
+                            echo "Error al subir el archivo.";
+                        }
+                    }
+                ?>
+                <p><strong>Imagen</strong>:</p><img class='mb20px' src='recibido/<?php echo $carga; ?>' />
             </div>
+            <div class="col"></div>
         </div>
-    </main>
+    </div>
+</main>
 <?php
-    include_once("footer.php")
+    include_once("footer.php");
+    mysqli_close($conexion);
 ?>
