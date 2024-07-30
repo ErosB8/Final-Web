429350e4b2d23bd25100eb6cb31a930e6281301c
diegonzz <diego.nunez@davinci.edu.ar>
2024-07-30 18:41:55 -0300

    dsa

--- FINAL/carrito.php
+++ FINAL/carrito.php
@@ -4,15 +4,14 @@
 ?>
     <main class="mt25px">
         <div class="centrado">
-            
+            <h3 id="titulo">Bienvenido al carrito</h3>
         </div>
         
-        <h3 id="titulo">Bienvenido al carrito</h3>
-
-    <?php 
+    <?php
     if ($conexion != NULL) {
 
-        $consulta = "SELECT id, nombre, precio, cantidad FROM productos";
+        $consulta = "SELECT carrito.id, carrito.cantidad, carrito.total, producto.nombre, producto.foto
+            FROM carrito JOIN  producto ON carrito.id_producto = producto.id";
         $respuesta = mysqli_query($conexion, $consulta);
 
         print "<div class='container'>";
@@ -24,8 +23,9 @@
                     <article>
                         <p>$fila[id]</p>
                         <p> - $fila[nombre] - </p>
-                        <p>Precio: $$fila[precio]</p>
+                        <p>Foto: <img src='$fila[foto]' alt='Foto del producto'></p>
                         <p>Cantidad: $fila[cantidad]</p>
+                        <p>Total: $$fila[total]</p>
                     </article>
                 </div>
             </div>
